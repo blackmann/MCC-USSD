d54548b785e5070c45f1922d4bd7fe414636c4b7
De-Great Yartey <yarteydegreat2@gmail.com>
2017-12-05 11:44:23 +0000

    Modularize

---
+++ api/registration.py
@@ -0,0 +1,71 @@
+from api.util import *
+
+
+def handle_registration(request, level):
+    state_branch = 2
+    next_level = int(level) + 1
+
+    # the person has provided constituency
+    if level == 1:
+        return {
+            "Message": "Please select a ID type.\n\n"
+                       "1. Voter's ID\n"
+                       "2. Membership ID",
+            "ClientState": "%d:%d" % (state_branch, next_level,),
+            "Type": RESPONSE_USSD
+        }
+
+    if level == 2:
+        user_id_type = request.data.get(MESSAGE)
+
+        return {
+            "Message": "Please enter your ID Number",
+            "ClientState": "%d:%d:%s" % (state_branch, next_level, user_id_type),
+            "Type": RESPONSE_USSD
+        }
+
+    if level == 3:
+        user_id = request.data.get(MESSAGE)
+        user_id_type = request.data.get(CLIENT_STATE).split(":")[2]
+
+        return {
+            "Message": "Please select a payment method to pay registration fee of GHS 3.00\n\n"
+                       "1. MTN Mobile Money\n"
+                       "2. Airtel Money",
+            "ClientState": "%d:%d:%s:%s" % (state_branch, next_level, user_id_type, user_id),
+            "Type": RESPONSE_USSD
+        }
+
+    if level == 4:
+        payment_option = request.data.get(MESSAGE)
+        if not (payment_option in [str(a) for a in range(1, 3)]):
+            return invalid_option_data
+
+        user_id = request.data.get(CLIENT_STATE).split(":")[3]
+        id_type = request.data.get(CLIENT_STATE).split(":")[2]
+
+        return {
+            "Message": "Please enter your mobile money phone number",
+            "ClientState": "%d:%d:%s:%s:%s" % (state_branch, next_level, id_type, user_id, payment_option),
+            "Type": RESPONSE_USSD
+        }
+
+    if level == 5:
+        mobile_number = request.data.get(MESSAGE)
+        if len(mobile_number) != 10:
+            return invalid_option_data
+
+        payment_option = request.data.get(CLIENT_STATE).split(":")[4]
+        user_id = request.data.get(CLIENT_STATE).split(":")[3]
+        id_type = request.data.get(CLIENT_STATE).split(":")[2]
+
+        ussd_number = request.data.get("Mobile")
+        request_payment(mobile_number, 3, ussd_number, get_network(payment_option), id_type, user_id, "Registration")
+
+        return {
+            "Message": "Thank you for applying for party updates. Please kindly confirm payment of mobile money phone "
+                       "to complete process. "
+                       " Charge is GHS 3.00",
+            "Type": RELEASE_USSD
+        }
+
---
+++ api/util.py
@@ -0,0 +1,71 @@
+import requests
+
+# Notes
+# ClientState is structured in the form 'branching_method':'current_level' then other data
+# follow -> 1:2:Degreat Yartey:Alogboshie:1:etc
+# this is used to determine which level the user has gotten to and what to show
+
+RELEASE_USSD = "Release"
+RESPONSE_USSD = "Response"
+
+CLIENT_STATE = "ClientState"
+MESSAGE = "Message"
+
+INITIAL_CLIENT_STATE = "1"
+
+OPTION_PAY_DUES_ORDINARY = "1"
+# OPTION_PAY_DUES_EXECUTIVE = "2"
+OPTION_REGISTER_MEMBER = "2"
+
+invalid_option_data = {
+    "Type": RELEASE_USSD,
+    "Message": "The option you selected is not valid. Please try again!"
+}
+
+
+def get_network(position):
+    return ["mtn", "airtel", "tigo"][int(position) - 1]
+
+
+def request_payment(mobile_number, amount, ussd_number, network, id_type, id_number, intent):
+    response = requests.post("https://payment.mypayutil.com/api/users/authenticate",
+                             data={"mobileNumber": "0000000006",
+                                   "password": "memberReg#Newdeveloper5"})
+
+    print("[Request Payment] Authenticating...")
+    if str(response.status_code).startswith("20"):
+        print("Authentication done, success")
+        json_response = response.json()
+        access_token = json_response['access_token']
+
+        actual_id = ["Voters", "Membership"][int(id_type) - 1]
+
+        request_body = {
+            "mobileNumber": mobile_number,
+            "source": "USSD",
+            "thirdPartyRef": "N/A",
+            "amount": amount,
+            "parameters": {
+                "ID Type": actual_id,
+                "ID Number": id_number,
+                "Intent": intent,
+                "USSD Number": ussd_number,
+            },
+            "service": "5a12c32e2adb093f1c8bf0f4"
+        }
+
+        print(request_body)
+        print(network)
+
+        headers = {
+            "Authorization": "Bearer %s" % access_token
+        }
+
+        res = requests.post("https://payment.mypayutil.com/api/merchants/payments/%s" % network,
+                            json=request_body,
+                            headers=headers)
+
+        print("Request made %d %s" % (res.status_code, res.text,))
+
+    else:
+        print("Failed %d" % response.status_code)
