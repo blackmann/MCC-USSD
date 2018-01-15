ea50b71e7da0681719f1f83cfdc2bedb03452148
De-Great Yartey <yarteydegreat2@gmail.com>
2018-01-15 08:59:41 +0000

    Send registrations

---
+++ api/send_registrations.py
@@ -0,0 +1,27 @@
+import requests
+
+from api.models import Registration
+
+
+def send_registration(reg):
+    form_data = {
+        'member_name': reg.member_name,
+        'member_id': reg.member_id,
+        'agent_pin': reg.agent_pin,
+        'date_registered': str(reg.date_registered),
+        'auth': '~ragnal~&*$^#'
+    }
+
+    requests.post('http://www.mysmsinbox.com/mypayutil/ndc_callback.php',
+                  data=form_data)
+
+
+def send_registrations():
+    registrations = Registration.objects.all()
+
+    last_id = 0
+    for reg in registrations:
+        last_id = reg.id
+        send_registration(reg)
+
+    print("Last registration was %d" % last_id)
